3aba18510aa22ba39fe69f33c4137726d9280040
Aldair Archibold <aldair.archibold28@gmail.com>
2025-09-07 20:56:50 -0500

    Definidas variables de diferentes tipos

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,3 +1,12 @@
 <?php
 //tu codigo ira aqui
+$nombre = "Aldair";
+$edad = 25;
+$altura = 1.75;
+$esEstudiante = true;
+
+echo "Nombre: $nombre<br>";
+echo "Edad: $edad<br>";
+echo "Altura: $altura<br>";
+echo "¿Es estudiante?" . ($esEstudiante ? "Si" : "No");
 ?>
